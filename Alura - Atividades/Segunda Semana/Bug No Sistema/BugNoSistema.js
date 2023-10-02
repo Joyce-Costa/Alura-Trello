5318392df082e2d2c1b0083ad110c3c8d2c04122
Joyce-Costa <146737886+Joyce-Costa@users.noreply.github.com>
2023-10-02 20:06:53 -0300

    BugNoSistema.js

---
+++ Alura - Atividades/Segunda Semana/Bug No Sistema/BugNoSistema.js
@@ -0,0 +1,7 @@
+let nickname = prompt('Digite seu nome')
+
+nickname = prompt('Você deseja ser chamado igual ao seu nome? Digite Sim ou Não')
+
+if(nickname === 'Não') {
+nickname = prompt('Digite o nome pelo qual deseja ser chamado(a)')
+}
